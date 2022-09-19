1fb2b27f4a65804767dac85d4d433e4cd1550447
chandram13 <mschandr@uci.edu>
2022-09-19 11:38:44 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Marvish Chandra
+#include <iostream>
+
+template <typename T>
+public static void recently_played_interface(Title,Publisher,ReleaseDate,HoursPlayed,FriendsAlsoPlayed)
+{
+    Title = "My Favorite Game";
+    Publisher = "My Favorite Game's Publisher";
+    ReleaseDate = "January 1, 2022";
+    HoursPlayed = 28;
+    FriendsAlsoPlayed = "First Friend, Second Friend";
+}
